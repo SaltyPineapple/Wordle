3d922e0b6f4c8d56c9edc15e4ebd64bbab8dbcea
Mark Pratt <mjpratt31@gmail.com>
2022-01-22 17:33:28 -0800

    Adding on screen keyboard

--- app/src/main/java/com/example/wordle/MainActivity.java
+++ app/src/main/java/com/example/wordle/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.wordle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button key_Q;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        key_Q = findViewById(R.id.key_Q);
+        key_Q.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                key_Q.setBackgroundColor(Color.RED);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/wordle/Word.java
+++ app/src/main/java/com/example/wordle/Word.java
@@ -2,4 +2,16 @@ package com.example.wordle;
 
 public class Word {
 
+    public final int LENGTH = 5;
+    public String word;
+
+
+    public Word(String word){
+        if(word.length() == LENGTH){
+            this.word = word;
+        }
+    }
+
+
+
 }
